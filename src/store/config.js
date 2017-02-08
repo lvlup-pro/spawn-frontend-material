e9c81f347dd75647eb4adc5154bb88a633a81c01
SystemZ <hello@systemz.pl>
2017-02-08 01:10:27 +0100

    Multi env

--- src/store/config.js
+++ src/store/config.js
@@ -1,5 +1,17 @@
-const config = {
+const configDev = {
+    apiUrl: 'http://127.0.0.1/v3/'
+}
+
+const configStage = {
+    apiUrl: 'https://demoapi.lvlup.pro/v3/'
+}
+
+const configProd = {
     apiUrl: 'https://api.lvlup.pro/v3/'
 }
 
-if (typeof module !== "undefined") module.exports = config;
+console.log(process.env.NODE_ENV)
+
+if (typeof module !== "undefined" && process.env.NODE_ENV == "development") module.exports = configDev;
+if (typeof module !== "undefined" && process.env.NODE_ENV == "staging") module.exports = configStage;
+if (typeof module !== "undefined" && process.env.NODE_ENV == "production") module.exports = configProd;
