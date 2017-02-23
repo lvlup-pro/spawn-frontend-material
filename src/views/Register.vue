dc28021a775480bc74e2878ce2d4859d84cb29cd
Libter <libter.pl@gmail.com>
2017-02-23 21:00:38 +0100

    Register page mockup, waiting for backend

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,63 @@
 <template>
     <div>
         <v-container>
+            <v-row class="mt-5">
+                <v-col lg3></v-col>
+                <v-col xs12 md6 lg6>
+                    <v-card class="mp-5">
+                        <v-card-text>
+                            <h5>{{ $t('user.register.header') }}</h5>
+                            <form v-on:submit.prevent="onSubmit">
+                                <v-text-input
+                                    :label="$t('user.fullname')"
+                                    :placeholder="$t('user.placeholder.fullname')"
+                                    v-model="fullname"
+                                ></v-text-input>
+                                <v-text-input
+                                    :label="$t('user.username')"
+                                    :placeholder="$t('user.placeholder.username')"
+                                    v-model="username"
+                                ></v-text-input>
+                                <v-text-input
+                                    :label="$t('user.email')"
+                                    :placeholder="$t('user.placeholder.email')"
+                                    v-model="email"
+                                ></v-text-input>
+                                <v-text-input
+                                    :label="$t('user.password')"
+                                    type="password"
+                                    :placeholder="$t('user.placeholder.password')"
+                                    v-model="password"
+                                ></v-text-input>
+                                <v-text-input
+                                    :label="$t('user.repeatpassword')"
+                                    type="password"
+                                    :placeholder="$t('user.placeholder.password')"
+                                    v-model="repeatpassword"
+                                ></v-text-input>
+                                <v-btn flat="flat" dark="dark" success block type="submit">
+                                    <v-icon left>vpn_key</v-icon>
+                                    {{$t('user.register.button')}}
+                                </v-btn>
+                            </form>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
 <script>
     export default {
+        data () {
+            return {
+                fullname: "",
+                username: "",
+                email: "",
+                password: "",
+                repeatpassword: ""
+            }
+        },
         mounted () {
             this.$emit('view', this.meta())
             this.$emit('redirectLang', 'register')
