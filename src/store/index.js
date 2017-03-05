a0c5dc99de15c1d55508209effaf1fed618008d2
Libter <libter.pl@gmail.com>
2017-03-05 13:41:37 +0100

    Use single quotes, remove semicolons

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,12 @@ import storeConfig from './config.js'
 
 function request({dispatch, state}, method, url, args, callback) {
     for (let key in args) {
-        let find = '{' + key + '}';
+        let find = '{' + key + '}'
         while (url.indexOf(find) !== -1) {
-            url = url.replace(find, args[key]);
+            url = url.replace(find, args[key])
         }
     }
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
     return method(state.apiUrl + url, args)
         .then(function(response) {
             return callback(response)
@@ -36,50 +36,50 @@ function put({dispatch, state}, url, args, callback) {
 
 export default new Vuex.Store({
     state: {
-        apiUrl: "",
+        apiUrl: '',
         token: null,
-        reCaptchaSiteKey: "",
+        reCaptchaSiteKey: '',
         loading: false,
         error: false,
         account: {
-            username: ""
+            username: ''
         },
         wallet: [
             {
-                balance_pretty: ""
+                balance_pretty: ''
             }
         ],
         ticket: {},
         vps: {},
         profile: {},
         ticketMessages: {},
-        services: {"paging": {"total_pages": 1}},
-        pagination: {"paging": {"total_pages": 1}},
+        services: {'paging': {'total_pages': 1}},
+        pagination: {'paging': {'total_pages': 1}},
         count: 1,
-        toolbarTitle: "",
+        toolbarTitle: '',
         toolbarTitleArgs: {},
-        version: "",
-        language: "",
+        version: '',
+        language: '',
         meta: {},
         logout: false
     },
     actions: {
         boot ({dispatch, state, commit}) {
-            commit('setVersion', "v" + storeConfig.version)
+            commit('setVersion', 'v' + storeConfig.version)
             commit('setReCaptchaSiteKey', storeConfig.reCaptchaSiteKey)
         },
         logOut ({dispatch, state, commit}) {
-            localStorage.removeItem("token");
+            localStorage.removeItem('token')
             commit('setAccount', {})
-            commit('setWallet', [{balance_pretty: ""}])
+            commit('setWallet', [{balance_pretty: ''}])
             commit('setServices', [])
             commit('setToken', null)
             commit('setError', false)
             commit('setLogout', true)
         },
         handleError({dispatch, state, commit}, args) {
-            let error = args.error, msg = error.message, response = error.response, url = args.name;
-            console.log(msg + ' while calling ' + url);
+            let error = args.error, msg = error.message, response = error.response, url = args.name
+            console.log(msg + ' while calling ' + url)
             if (typeof response !== 'undefined') {
                 switch (response.status) {
                     case 401:
@@ -109,13 +109,13 @@ export default new Vuex.Store({
                 return true
             }
             //no token, force login
-            if (localStorage.getItem("token") === null) {
+            if (localStorage.getItem('token') === null) {
                 return true
             } else {
                 dispatch('accountInfo').then((res) => {
-                    //if no proper response then "log out" and force new login
+                    //if no proper response then 'log out' and force new login
                     if (res !== true) {
-                        localStorage.removeItem("token");
+                        localStorage.removeItem('token')
                         return true
                     }
                 })
@@ -125,8 +125,8 @@ export default new Vuex.Store({
         accountLogin ({commit, dispatch, state}, args) {
             return post({dispatch, state}, 'auth/login', args, function (res) {
                 if (typeof res.data.token !== 'undefined') {
-                    localStorage.setItem('token', res.data.token);
-                    return true;
+                    localStorage.setItem('token', res.data.token)
+                    return true
                 }
             })
         },
@@ -154,7 +154,7 @@ export default new Vuex.Store({
             return get({dispatch, state}, 'help/ticket/{id}', args, function (res) {
                 if (typeof res.data.id !== 'undefined') {
                     //http://stackoverflow.com/a/784547/1351857
-                    res.data.message = res.data.message.replace(/(?:\r\n|\r|\n)/g, '<br>');
+                    res.data.message = res.data.message.replace(/(?:\r\n|\r|\n)/g, '<br>')
                     commit('setTicket', res.data)
                 }
             })
@@ -197,31 +197,31 @@ export default new Vuex.Store({
             return get({dispatch, state}, 'vps/{id}', args, function (res) {
                 if (typeof res.data.status !== 'undefined') {
                     if (typeof state.vps.status !== 'undefined' && state.vps.status === 'running') {
-                        let s = res.data.uptime_s - state.vps.uptime_s;
+                        let s = res.data.uptime_s - state.vps.uptime_s
                         if (s > 0) {
-                            let netInChange = res.data.net_in_b - state.vps.net_in_b;
-                            let netOutChange = res.data.net_out_b - state.vps.net_out_b;
-                            res.data.net_in_bps = netInChange / s;
-                            res.data.net_out_bps = netOutChange / s;
+                            let netInChange = res.data.net_in_b - state.vps.net_in_b
+                            let netOutChange = res.data.net_out_b - state.vps.net_out_b
+                            res.data.net_in_bps = netInChange / s
+                            res.data.net_out_bps = netOutChange / s
                             if (state.vps.virt == 'kvm') {
-                                let diskReadChange = res.data.disk_read_b - state.vps.disk_read_b;
-                                let diskWriteChange = res.data.disk_write_b - state.vps.disk_write_b;
-                                res.data.disk_read_bps = diskReadChange / s;
-                                res.data.disk_write_bps = diskWriteChange / s;
+                                let diskReadChange = res.data.disk_read_b - state.vps.disk_read_b
+                                let diskWriteChange = res.data.disk_write_b - state.vps.disk_write_b
+                                res.data.disk_read_bps = diskReadChange / s
+                                res.data.disk_write_bps = diskWriteChange / s
                             }
                         } else {
-                            res.data.net_in_bps = state.vps.net_int_bps;
-                            res.data.net_out_bps = state.vps.net_out_bps;
+                            res.data.net_in_bps = state.vps.net_int_bps
+                            res.data.net_out_bps = state.vps.net_out_bps
                             if (state.vps.virt == 'kvm') {
-                                res.data.disk_read_bps = state.vps.disk_read_bps;
-                                res.data.disk_write_bps = state.vps.disk_write_bps;
+                                res.data.disk_read_bps = state.vps.disk_read_bps
+                                res.data.disk_write_bps = state.vps.disk_write_bps
                             }
                         }
                     } else {
-                        res.data.net_in_bps = -1;
-                        res.data.net_out_bps = -1;
-                        res.data.disk_read_bps = -1;
-                        res.data.disk_write_bps = -1;
+                        res.data.net_in_bps = -1
+                        res.data.net_out_bps = -1
+                        res.data.disk_read_bps = -1
+                        res.data.disk_write_bps = -1
                     }
                     res.data.ip = state.vps.ip
                     commit('setVps', res.data)
@@ -258,31 +258,31 @@ export default new Vuex.Store({
     },
     mutations: {
         setVersion (state, newVersion) {
-            state.version = newVersion;
+            state.version = newVersion
         },
         setToken (state, newToken) {
-            state.token = newToken;
+            state.token = newToken
         },
         setError (state, newErrorState) {
-            state.error = newErrorState;
+            state.error = newErrorState
         },
         setReCaptchaSiteKey (state, newKey) {
-            state.reCaptchaSiteKey = newKey;
+            state.reCaptchaSiteKey = newKey
         },
         setUsername (state, newUsername) {
-            state.account.username = newUsername;
+            state.account.username = newUsername
         },
         setAccount (state, newAccount) {
-            state.account = newAccount;
+            state.account = newAccount
         },
         setWallet (state, newWallet) {
-            state.wallet = newWallet;
+            state.wallet = newWallet
         },
         setTicket (state, newTicket) {
-            state.ticket = newTicket;
+            state.ticket = newTicket
         },
         setVps (state, newVps) {
-            state.vps = newVps;
+            state.vps = newVps
         },
         setVpsIps(state, newIps) {
             state.vps.ip = {}
@@ -293,44 +293,44 @@ export default new Vuex.Store({
             state.profile = newProfile
         },
         setTicketMessages (state, newTicketMessages) {
-            state.ticketMessages = newTicketMessages;
+            state.ticketMessages = newTicketMessages
         },
         setServices (state, newServices) {
-            state.services = newServices;
+            state.services = newServices
         },
         setPagination (state, newPagination) {
-            state.pagination = newPagination;
+            state.pagination = newPagination
         },
         setToolbarTitle (state, newTitle) {
-            state.toolbarTitle = newTitle;
+            state.toolbarTitle = newTitle
         },
         setToolbarTitleArgs (state, newTitleArgs) {
-            state.toolbarTitleArgs = newTitleArgs;
+            state.toolbarTitleArgs = newTitleArgs
         },
         setLanguage (state, newLanguage) {
-            state.language = newLanguage;
-            Vue.config.lang = newLanguage;
+            state.language = newLanguage
+            Vue.config.lang = newLanguage
         },
         setMeta(state, newMeta) {
-            state.meta = newMeta;
+            state.meta = newMeta
         },
         setLoading (state) {
-            state.loading = true;
+            state.loading = true
         },
         setLoaded (state) {
-            state.loading = false;
+            state.loading = false
         },
         setLogout(state, newLogout) {
-            state.logout = newLogout;
+            state.logout = newLogout
         },
         setTitle (state, newTitle) {
-            document.title = newTitle;
+            document.title = newTitle
         },
         setDescription (state, newDescription) {
-            document.head.querySelector('meta[name=description]').content = newDescription;
+            document.head.querySelector('meta[name=description]').content = newDescription
         },
         setKeywords (state, newKeywords) {
-            document.head.querySelector('meta[name=keywords]').content = newKeywords;
+            document.head.querySelector('meta[name=keywords]').content = newKeywords
         }
     }
 })
