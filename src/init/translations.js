b8c7a6b28feba6aa3e32a837f9da8f7a48d7a757
Libter <libter.pl@gmail.com>
2017-03-09 21:56:22 +0100

    Split part of app.js to init/components.js, init/filters.js, init/translations.js

---
+++ src/init/translations.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import VeeValidate from 'vee-validate'
+
+let en = require('../store/lang/en.json'),
+    pl = require('../store/lang/pl.json'),
+    de = require('../store/lang/de.json'),
+    es = require('../store/lang/es.json');
+
+Vue.use(VueI18n)
+Vue.locale('en', en)
+Vue.locale('pl', pl)
+Vue.locale('de', de)
+Vue.locale('es', es)
+
+function toLambdas(obj) {
+    let newObj = {}
+    for (let key in obj) {
+        newObj[key] = () => obj[key]
+    }
+    return newObj
+}
+
+Vue.use(VeeValidate, {
+    locale: 'en',
+    dictionary: {
+        pl: {
+            messages: toLambdas(pl.validation)
+        },
+        en: {
+            messages: toLambdas(en.validation)
+        },
+        de: {
+            messages: toLambdas(en.validation)
+        },
+        es: {
+            messages: toLambdas(es.validation)
+        }
+    }
+})
