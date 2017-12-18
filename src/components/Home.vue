79ec70ba938cba21e7a1698edc22e946ea4a65b4
SystemZ <hello@systemz.pl>
2017-12-18 22:05:35 +0100

    More stylish homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,12 @@
   <v-container>
     <v-slide-y-transition mode="out-in">
       <v-layout column>
-        {{ $t("hello") }}
-        <h2>Forum</h2>
-        <discourse-latest-posts ></discourse-latest-posts>
+        <div class="display-3 grey--text text--darken-1">{{ $t('news') }}</div>
+        <div class="grey--text text--darken-1 mb-3">
+          {{ $t("hello") }}
+        </div>
+        <br><br>
+        <discourse-latest-posts></discourse-latest-posts>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -15,10 +18,12 @@
     i18n: {
       messages: {
         en: {
-          hello: 'Welcome to lvlup.pro customer panel v3! Below you can find out what is new in our community:'
+          news: 'News',
+          hello: 'Welcome to lvlup.pro customer panel v3! Below you can find out what is new in our community'
         },
         pl: {
-          hello: 'Witaj w panelu klienta lvlup.pro v3. Poniżej możesz zapoznać się z nowościami naszej społeczności:'
+          news: 'Nowości',
+          hello: 'Witaj w panelu klienta lvlup.pro v3. Poniżej możesz zapoznać się z nowościami naszej społeczności'
         }
       }
     },
